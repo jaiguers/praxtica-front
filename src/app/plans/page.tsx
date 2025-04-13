82d46a8886c582321c2317e20595e72f4be6e4f5
Jair Guerrero <jaiguers@outlook.com>
2025-04-12 23:00:05 -0500

    [UPDATE] Modificar la pagina de de planes

--- src/app/plans/page.tsx
+++ src/app/plans/page.tsx
@@ -105,7 +105,7 @@ export default function Plans() {
                 <svg className="w-5 h-5 mr-2 text-green-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
                 </svg>
-                2 meses gratis
+                Una sorpresa
               </li>
               <li className="flex items-center">
                 <svg className="w-5 h-5 mr-2 text-green-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
